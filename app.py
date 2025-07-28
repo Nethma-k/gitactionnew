b922f8347da0eb2d4db098053a3f3f5b08580d18
Nethma-k <111905294+Nethma-k@users.noreply.github.com>
2025-07-28 13:13:39 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello this will be deployed using GitHub Actions'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
